734417d338f18981e39efa2d1fce5a205a9111aa
Igor Mladenovic <1839481+mligor@users.noreply.github.com>
2024-06-13 01:06:45 +0200

    RA-2 Add bootstrap icons and dark mode

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -17,12 +17,14 @@ export class DashboardComponent {
       title: 'Clients / Applications',
       description: 'View and manage all registered clients and applications authorized to access the OAuth2 server.',
       route: '/clients',
+      icon: 'window-fullscreen',
     },
     {
       title: 'User Pools',
       description:
-        'View and manage all user pools, including user accounts, authentication settings, and security configurations',
+        'View and manage all user pools, including user accounts, authentication settings, and security configurations.',
       route: '/user-pools',
+      icon: 'people',
     },
   ];
 }
